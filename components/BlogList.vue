26033ffd0cd0d1ca57297ce998d9ab8d5878956c
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-05-27 01:24:06 +0200

    feat(content): add ContentList

---
+++ components/BlogList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <ContentList path="/blog">
+      <template #default="{ list }">
+        {{ list }}
+        <div v-for="article in list" :key="article._path">
+          <h2>{{ article.title }}</h2>
+          <p>{{ article.description }}</p>
+        </div>
+      </template>
+      <template #not-found>
+        <p>No articles found</p>
+      </template>
+    </ContentList>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  hi
+  <BlogList />
 </template>
