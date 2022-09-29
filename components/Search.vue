b87a083a0e38efc9a044ba4d412ffdeaa70d2b2a
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-09-30 00:50:45 +0200

    debounce search

--- components/Search.vue
+++ components/Search.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 const search = useSearch()
+const input = ref('')
+
+let debounce = null as NodeJS.Timeout | null
+function debounceSearch() {
+  if (debounce !== null)
+    clearTimeout(debounce)
+
+  debounce = setTimeout(() => {
+    search.input = input.value
+  }, 500)
+}
 </script>
 
 <template>
   <label bg-highlight rounded-2xl text-primary px-4 py-3 flex items-center gap-2>
     <div i-fa6-solid-magnifying-glass text-lg />
-    <input v-model="search.input" type="text" placeholder="Suche" spellcheck="false" w-full bg-transparent outline-none>
+    <input v-model="input" type="text" placeholder="Suche" spellcheck="false" w-full bg-transparent outline-none @input="debounceSearch">
   </label>
 </template>
--- components/blog/BlogList.vue
+++ components/blog/BlogList.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 const search = useSearch()
-const query = ref(await queryContent('blog').where({ $or: [{ title: { $icontains: search.input } }, { description: { $contains: search.input } }] }).find())
+const query = ref()
 
-watch(search, async (search) => {
+watchEffect(async () => {
   query.value = await queryContent('blog').where({ $or: [{ title: { $icontains: search.input } }, { description: { $contains: search.input } }] }).find()
 })
 </script>
@@ -12,9 +12,6 @@ watch(search, async (search) => {
     <h2 font-black text-lg>
       Aktuelle Beiträge
     </h2>
-    <!-- {{ query }} -->
-    <!-- TODO: Fix https://github.com/nuxt/content/issues/1570 -->
-
     <div v-for="article in query" :key="article._path">
       <NuxtLink :to="article._path">
         <h2 text-xl font-bold>
@@ -23,26 +20,5 @@ watch(search, async (search) => {
       </NuxtLink>
       <p>{{ article.description }}</p>
     </div>
-
-    <!-- <ContentList :query="query" path="/blog">
-      <template #default="{ list }">
-        <div v-for="article in list" :key="article._path">
-          ---
-          {{ article }}
-          <NuxtLink :to="article._path">
-            <h2 text-xl font-bold>
-              {{ article.title }}
-            </h2>
-          </NuxtLink>
-          <p>{{ article.description }}</p>
-        </div>
-      </template>
-      <template #empty>
-        <p>No articles found</p>
-      </template>
-      <template #not-found>
-        <p>No articles found</p>
-      </template>
-    </ContentList> -->
   </div>
 </template>
