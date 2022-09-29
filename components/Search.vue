5059d313719caa70255b6308d2a822acff85281b
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-09-30 01:18:11 +0200

    trigger search on enter

--- components/Search.vue
+++ components/Search.vue
@@ -3,19 +3,27 @@ const search = useSearch()
 const input = ref('')
 
 let debounce = null as NodeJS.Timeout | null
+
 function debounceSearch() {
   if (debounce !== null)
     clearTimeout(debounce)
 
   debounce = setTimeout(() => {
     search.input = input.value
-  }, 500)
+  }, 300)
+}
+
+function triggerSearch() {
+  if (debounce !== null)
+    clearTimeout(debounce)
+
+  search.input = input.value
 }
 </script>
 
 <template>
   <label bg-highlight rounded-2xl text-primary px-4 py-3 flex items-center gap-2>
     <div i-fa6-solid-magnifying-glass text-lg />
-    <input v-model="input" type="text" placeholder="Suche" spellcheck="false" w-full bg-transparent outline-none @input="debounceSearch">
+    <input v-model="input" type="text" placeholder="Suche" spellcheck="false" w-full bg-transparent outline-none @input="debounceSearch" @enter="triggerSearch">
   </label>
 </template>
