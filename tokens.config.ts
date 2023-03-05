3c1b9269bd473397ecbf9d94bc505f352fe7fbb3
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-03-05 21:01:33 +0100

    feat(typography): reduce size for p, br

--- tokens.config.ts
+++ tokens.config.ts
@@ -3,13 +3,17 @@ import { defineTheme } from 'pinceau'
 export default defineTheme({
   typography: {
     verticalMargin: {
-      xs: '8px',
-      sm: '16px',
-      base: '32px',
+      xs: '4px',
+      sm: '8px',
+      base: '16px',
+    },
+    fontSize: {
+      base: '16px',
     },
   },
   prose: {
     p: {
+      fontSize: '{typography.fontSize.base}',
       br: {
         margin: '{typography.verticalMargin.xs} 0 0 0',
       },
